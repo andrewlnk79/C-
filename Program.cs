81bab506d5d2c8664ac8681b7440666a7dd5a884
AndreyLinnik <av.linnik79@ya.ru>
2022-08-06 11:41:05 +0300

    home work1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿Console.Write("ввелите число a=");
+int number, mnumber1,
+number = int.Parse(Console.ReadLine());
+Console.Write("ввелите число b=");
+number2 = int.Parse(Console.ReadLine());
+
