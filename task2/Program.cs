f29919cd11af39630e5a2436bd2b44371ad5345c
AndreyLinnik <av.linnik79@ya.ru>
2022-08-06 14:53:05 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("число1=");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("число2=");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine((num1 > num2) ? "число1 больше" : "число1 меньше");
